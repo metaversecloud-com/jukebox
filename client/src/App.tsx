001ecceaa6d816914d4378bc2e233df8bfe69041
abel-tefera <abeltefera16@gmail.com>
2024-02-24 13:18:54 +0300

    Client boilerplate

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,34 @@
+import { Route, Routes, useSearchParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+import Home from "@/pages/Home";
+import Error from "@/pages/Error";
+import { useEffect } from "react";
+
 const App = () => {
-  return <>Jukebox</>;
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const interactiveParams = {
+      assetId: searchParams.get("assetId"),
+      displayName: searchParams.get("displayName"),
+      interactiveNonce: searchParams.get("interactiveNonce"),
+      interactivePublicKey: searchParams.get("interactivePublicKey"),
+      profileId: searchParams.get("profileId"),
+      sceneDropId: searchParams.get("sceneDropId"),
+      uniqueName: searchParams.get("uniqueName"),
+      urlSlug: searchParams.get("urlSlug"),
+      username: searchParams.get("username"),
+      visitorId: searchParams.get("visitorId"),
+    };
+  }, [searchParams]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 };
 
 export default App;
