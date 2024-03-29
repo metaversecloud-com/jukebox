414d7bb26f45ec7b72fe14cf1fe2fc2111c34f1c
abel-tefera <abeltefera16@gmail.com>
2024-03-29 18:39:03 +0300

    temp very large connect timeout

--- server/redis/index.ts
+++ server/redis/index.ts
@@ -16,21 +16,21 @@ const shouldSendEvent = (
 const redisObj = {
   publisher: createClient({
     url: process.env.REDIS_URL,
-    // connect_timeout: 1,
+    connect_timeout: 1,
     // password: process.env.REDIS_PASSWORD,
     socket: {
       tls: process.env.REDIS_URL.startsWith("rediss"),
-      connectTimeout: 5000
+      connectTimeout: 50000
       // port: parseInt(process.env.REDIS_PORT) || 6379,
     },
   }),
   subscriber: createClient({
     url: process.env.REDIS_URL,
-    // connect_timeout: 1,
+    connect_timeout: 1,
     // password: process.env.REDIS_PASSWORD,
     socket: {
       tls: process.env.REDIS_URL.startsWith("rediss"),
-      connectTimeout: 5000
+      connectTimeout: 50000
       // port: parseInt(process.env.REDIS_PORT) || 6379,
     },
   }),
